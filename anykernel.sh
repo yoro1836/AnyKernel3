6408f0b6282a211558f2eeff5b67216a22dd1c8d
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2025-08-12 11:00:07 +0600

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=KernelSU by KernelSU Developers
+kernel.string=KernelSU-Next GKI Kernel by KernelSU-Next Developers
 do.devicecheck=0
 do.modules=0
 do.systemless=0
